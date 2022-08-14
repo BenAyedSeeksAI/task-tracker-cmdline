c0cb575813f64771904309026606c8a0262b5044
Mohamed Fares Ben ayed <faresaction@yahoo.fr>
2022-08-14 14:06:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import typer
+from rich.console import Console
+from rich.table import Table
+
+cnsle = Console()
+app = typer.Typer()
+
+tasks = [("start writing markdowns","work related"),("update github","work related")]
+
+def get_category_color(category):
+        COLORS = {'Learn': 'cyan', 'work related': 'red', 'Sports': 'cyan', 'Study': 'green'}
+        if category in COLORS:
+            return COLORS[category]
+        return 'white'
+
+def formatTable():
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("id", style="dim", width=6)
+    table.add_column("Todo", min_width=20)
+    table.add_column("Category", min_width=12, justify="right")
+    return table
+
+@app.command(short_help = "add an item")
+def add(task: str, category: str):
+    typer.echo(f"adding task -{task}- of category -{category}-")
+
+@app.command(short_help= 'show')
+def show():
+    cnsle.print("[bold magenta]Todos[/bold magenta]!", "💻")
+    table = formatTable()
+    for idx, data in enumerate(tasks, start= 1):
+        color = get_category_color(data[1])
+        table.add_row(str(idx),
+                      data[0],
+                      f"[{color}]{data[1]}[/{color}]"
+                      )
+    cnsle.print(table)
+
+if __name__ == "__main__":
+    app()
+
